ea0b962d55f7f44d5efdae1a6756b4d50c46379a
Artilino <cristian.qprog@gmail.com>
2024-08-05 11:09:24 +0200

    "Fixed SplitScreenR responsive"

--- src/components/SplitScreenR/SplitScreenR.jsx
+++ src/components/SplitScreenR/SplitScreenR.jsx
@@ -10,25 +10,36 @@ const SplitScreenL = ({ data }) => {
       <div className="container">
         <div className="scl-container">
 
-          {/* IMage Section */}
-          <div className="scl-imageSection">
-            <Image src={data.imageUrl} alt={data.imageAlt} layout="fill" objectFit="cover" />
-          </div>
 
-          {/* Text Section */}
-          <div className="scl-textSection">
-            <h1>{data.title}</h1>
-            {data.paragraphs.map((paragraph, index) => (
-              <p key={index}>{paragraph}</p>
-            ))}
-            <div className="scl-button-container">
-              <button className="scl-button">
-                {data.buttonText}<span className="arrow-icon">{">>"}</span>
-              </button>
+          <div className="scl-blocks">
+
+
+            {/* IMage Section */}
+            <div className="scl-block">
+              <div className="scl-imageSection">
+                <Image src={data.imageUrl} alt={data.imageAlt} layout="fill" objectFit="cover" />
+              </div>
             </div>
-          </div>
 
-          
+            
+            {/* Text Section */}
+            <div className="scl-block">
+              <div className="scl-textSection">
+                <h1>{data.title}</h1>
+                {data.paragraphs.map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+                <div className="scl-button-container">
+                  <button className="scl-button">
+                    {data.buttonText}<span className="arrow-icon">{">>"}</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            
+
+          </div>
         </div>
       </div>
     </div>
