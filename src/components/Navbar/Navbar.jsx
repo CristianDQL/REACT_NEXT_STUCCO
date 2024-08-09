91f9368324d11f95a80c84f6fc682287fe2cd0e4
Artilino <cristian.qprog@gmail.com>
2024-08-09 15:35:37 +0200

    "add linked menu desktop"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,32 @@ import './Navbar.css';
 import { BiMenuAltRight } from 'react-icons/bi'; 
 import { RxCross2 } from 'react-icons/rx';
 import { motion, useMotionValueEvent, useScroll } from "framer-motion"; /* Sticky menu */
-import { Link } from "react-scroll"; /* Menu clickable */
+
 import Image from 'next/image';
 
+/* links */
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+    { name: 'Home', href: '/'},
+    {
+      name: 'Services', href: '/services'
+    },
+    { name: 'Portfolio', href: '/portfolio'},
+    { name: 'Contact', href: '/contact'},
+  ];
+
+ 
+
 const Navbar = () => {
+    
+    const pathname = usePathname();
+
+
     {/*State Hook - funcionalidad para abrir el menú hamburguesa con clicl*/}
     const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
 
@@ -24,7 +46,10 @@ const Navbar = () => {
         }
     });
 
+
+
   return (
+    
     //n = navbar-wrapper, short n for navbar
     //Esta concatenación de divs es para faciltiar el Responsive
     <div className={`n-wrapper ${navStyle}`}>
@@ -35,20 +60,33 @@ const Navbar = () => {
                 {/*Dos secciones para el navbat*/}
                 {/*Left side */}
                 <div className="n-logo">
-                <img src="NavBar/optimum-stucco-services-logo.svg" alt="LOGO"  />
+                 <img src="NavBar/optimum-stucco-services-logo.svg" alt="LOGO"  />
                 </div>
 
                 {/*Right side*/}
                 <div className="n-right">
                     <div className="n-menu">
-                        {/*4 spans para cada submenu del menu*/}
-                        {/* Sin autoscrolling */}
-                        {/* <span>Services</span>
-                        <span>Portfolio</span>
-                        <span>Details</span>
-                        <span>Contact Us </span> */}
+                        
+                    {links.map((link) => (
+                        <Link 
+                            key={link.name} 
+                            href={link.href}
+                            
+                            className={clsx(
+                                'link-style',
+                                { 'active-link': pathname === link.href }
+                            )}
+                                
+                        >   
+                            <p className="hidden md:block">{link.name}</p>
+                        </Link>
+                    ))}
 
-                        <Link to="wwd-wrapper" spy={true} smooth={true}>
+
+
+
+
+                        {/* <Link to="wwd-wrapper" spy={true} smooth={true}>
                             <span>HOME</span>
                         </Link>
                         <Link to="hiw-wrapper" spy smooth offset={100}>
@@ -59,7 +97,7 @@ const Navbar = () => {
                         </Link>
                         <Link to="t-wrapper" spy smooth offset={100}>
                             <span>DETAILS </span>
-                        </Link>
+                        </Link> */}
                         
                     </div>
                     <div className="fund-button">CONTACT US</div>
@@ -139,6 +177,6 @@ const Navbar = () => {
 
     </div>
   );
-};
+}
 
 export default Navbar
